7d27eaa612134411a2d658bfba62bc417e14a8d5
Mehul-Mer <mehulmer2303@gmail.com>
2026-06-01 08:08:14 +0530

    Patterns add

---
+++ Patterns/AbcPattern.java
@@ -0,0 +1,14 @@
+public class AbcPattern {
+
+    public static void main(String[] args){
+        int n = 5;
+        for(int i = n - 1; i >= 0; i--){
+            char c = 'A';
+            for(int j = 0; j <= i; j++){
+                System.out.print(c);
+                c++;
+            }
+            System.out.println();
+        }
+    }
+}
