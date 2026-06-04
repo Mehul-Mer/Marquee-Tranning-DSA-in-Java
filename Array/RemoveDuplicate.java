e0bf34ad3b20ea62a080f163c3f01d0df7fbe1c7
Mehul-Mer <mehulmer2303@gmail.com>
2026-06-04 10:58:56 +0530

    new file

---
+++ Array/RemoveDuplicate.java
@@ -0,0 +1,12 @@
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,1,2,3,4,5};
+        for(int i = 0; i<arr.length ; i++){
+            for(int j = 0; j<arr.length ; j++){
+                if(arr[i] == arr[j]){
+                    
+                }
+            }
+        }
+    }
+}
